df19ce6a52bb2e248b1c3935b1ad6f216ab628ff
Pedro Lucas <pl.lucassouza@gmail.com>
2024-06-13 08:49:52 -0300

    Adicionando a tela de leitura de email

--- app/src/main/java/br/com/fiap/email/MainActivity.kt
+++ app/src/main/java/br/com/fiap/email/MainActivity.kt
@@ -55,7 +55,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Login()
+                    LeituraEmail()
                 }
             }
         }
@@ -307,7 +307,6 @@ fun Cadastro() {
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-//                    .background(colorResource(id = R.color.purple_200))
             ){
                 Spacer(modifier = Modifier.height(16.dp))
 
@@ -464,6 +463,137 @@ fun Cadastro() {
             }
         }
 
+    }
+}
+
+@Composable
+fun LeituraEmail(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp, 28.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+                    contentDescription = "icon arrow back",
+                    modifier = Modifier.size(30.dp)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_delete_24),
+                    contentDescription = "delete icon",
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+        }
+        
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp, 20.dp)
+        ) {
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "Perfil",
+                    modifier = Modifier.size(50.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Column {
+                    Text(
+                        text = "Carolina",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text(
+                        text = "13:56",
+                        fontSize = 10.sp,
+                        color = Color.Black
+                    )
+                }
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp, 15.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Text(
+                    text = "Reunião",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Row(){
+                    Image(
+                        painter = painterResource(id = R.drawable.baseline_calendar_month_24),
+                        contentDescription = "Calendar Icon"
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.baseline_keyboard_return_24),
+                        contentDescription = "Responder Icon"
+                    )
+                }
+            }
+        }
+
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp, 15.dp)
+        ){
+            Text(
+                text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                        " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s," +
+                        " when an unknown printer took a galley of type and scrambled it to make a type" +
+                        " specimen book. It has survived not only five centuries, but also the leap into electronic typesetting," +
+                        " remaining essentially unchanged. It was popularised in the 1960s with the release of" +
+                        " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop " +
+                        "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                color = Color.Black
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp, 15.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Button(
+                    shape = RoundedCornerShape(5
+                        .dp),
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00ACC8))
+                ) {
+                    Text(
+                        text = "Responder com IA",
+                        color = Color.White
+                    )
+
+                }
+            }
+        }
     }
 }
 
